b5ad35a5b8eea2e289c871ee5b4a2521fab01ded
radioneko <kawaii.neko@pochta.ru>
2012-10-27 15:41:39 +0400

    Import expat library

---
+++ autogen.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+touch NEWS ChangeLog
+
+echo "Running aclocal..." ; aclocal $ACLOCAL_FLAGS || exit 1
+echo "Running autoheader..." ; autoheader || exit 1
+echo "Running autoconf..." ; autoconf || exit 1
+#echo "Running libtoolize..." ; (libtoolize --copy --automake || glibtoolize --automake) || exit 1
+echo "Running automake..." ; automake --add-missing --copy || exit 1
+./configure ${*}
